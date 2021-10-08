eaf102d5dea4b5a3fe5c128e8154a909e37fbfb7
Rishabh  Pratap <pratap001rishabh@gmail.com>
2021-10-08 23:20:26 +0530

    added api url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ChipMonks-Interview';
+  api = 'https://reqres.in/api/users';
 
   constructor (http: HttpClient) {}
 }
